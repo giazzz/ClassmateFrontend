da04e76abb91a6812d9761048d0af3580d5f78c0
nguyentv <tvngiagia@gmail.com>
2020-08-31 00:43:35 +0700

    Add layout.

--- src/app/views/classwork/classwork.component.ts
+++ src/app/views/classwork/classwork.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-classwork',
@@ -13,12 +14,26 @@ export class ClassworkComponent implements OnInit {
 
   ngOnInit(): void {
     this.lstClasswork = [
-      {title: 'Test1', createAt: '09:00 28/08/2020'},
-      {title: 'Test1', createAt: '09:00 28/08/2020'},
-      {title: 'Test1', createAt: '09:00 28/08/2020'},
-      {title: 'Test1', createAt: '09:00 28/08/2020'},
-      {title: 'Test1', createAt: '09:00 28/08/2020'}
+      {id: '1', title: 'Test1', createAt: '09:00 28/08/2020'},
+      {id: '2', title: 'Test1', createAt: '09:00 28/08/2020'},
+      {id: '3', title: 'Test1', createAt: '09:00 28/08/2020'},
+      {id: '4', title: 'Test1', createAt: '09:00 28/08/2020'},
+      {id: '5', title: 'Test1', createAt: '09:00 28/08/2020'},
     ];
+  }
+
+  collapse(blnShow: boolean, id: string, e) {
+    const item: HTMLElement = e.target;
+    if (!item.classList.contains('icon-options-vertical')) {
+      $('.div-collapse').addClass('d-none');
+      $('.exam').removeClass('is-hover');
+      if (blnShow) {
+        $('#col-' + id).removeClass('d-none');
+        $('#exam-' + id).addClass('is-hover');
+      } else {
+        $('#col-' + id).addClass('d-none');
+      }
+    }
   }
 
 }
--- src/app/views/mark/mark.component.ts
+++ src/app/views/mark/mark.component.ts
@@ -7,9 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarkComponent implements OnInit {
 
+  public lstClasswork: any[];
+  public lstStudent: any[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.lstClasswork = [
+      {id: '1', title: 'Test1', createAt: '09:00 28/08/2020'},
+      {id: '2', title: 'Test2', createAt: '09:00 28/08/2020'},
+      {id: '3', title: 'Test3', createAt: '09:00 28/08/2020'},
+      {id: '4', title: 'Test4', createAt: '09:00 28/08/2020'},
+      {id: '5', title: 'Test5', createAt: '09:00 28/08/2020'},
+    ];
+
+    this.lstStudent = [
+      {
+        id: '1',
+        name: 'Trần Văn Nguyên',
+        lstScore: [
+          {id: '1', title: 'Test1', score: 6},
+          {id: '2', title: 'Test2', score: 7},
+          {id: '3', title: 'Test3', score: 8},
+          {id: '4', title: 'Test4', score: 9},
+          {id: '5', title: 'Test5', score: 10},
+        ]
+      },
+      {
+        id: '1',
+        name: 'Trần Văn Nguyên',
+        lstScore: [
+          {id: '1', title: 'Test1', score: 6},
+          {id: '2', title: 'Test2', score: 7},
+          {id: '3', title: 'Test3', score: 8},
+          {id: '4', title: 'Test4', score: 9},
+          {id: '5', title: 'Test5', score: 10},
+        ]
+      },
+      {
+        id: '1',
+        name: 'Trần Văn Nguyên',
+        lstScore: [
+          {id: '1', title: 'Test1', score: 6},
+          {id: '2', title: 'Test2', score: 7},
+          {id: '3', title: 'Test3', score: 8},
+          {id: '4', title: 'Test4', score: 9},
+          {id: '5', title: 'Test5', score: 10},
+        ]
+      },
+    ];
   }
 
 }
