488951dbad46f418cd94430918e1a1edeb85f9b1
nguyentv <tvngiagia@gmail.com>
2020-09-17 22:32:38 +0700

    U

---
+++ src/app/views/mark/mark.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class MarkService {
+    headers;
+
+    constructor(private http: HttpClient) {
+        this.headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+          });
+    }
+
+    // getAllCourse() {
+    //     return this.http.get<any>(`${environment.apiUrl}/data/course/all`,
+    //         { headers: this.headers, observe: 'response' });
+    // }
+
+    getListAllExcercise(course_id) {
+        let params = new HttpParams();
+        params = params.append('course_id', course_id);
+        return this.http.get<any>(`${environment.apiUrl}/data/exercise/gradeList`,
+            { params, observe: 'response' });
+    }
+
+}
